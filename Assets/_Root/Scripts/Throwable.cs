8fd04dba65ceb8174d4577c85a44b7036c878493
Fernando Soares <fernando.soares@virtuleap.com>
2022-04-06 11:26:09 +0100

    Implement throw and knockable cyllinders

---
+++ Assets/_Root/Scripts/Throwable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class Throwable : OVRGrabbable
+{
+    [Header("Throw Values")]
+    [SerializeField] float _throwSpeedMultiplier = 2f;
+
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        base.GrabEnd(linearVelocity, angularVelocity);
+        
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(linearVelocity * _throwSpeedMultiplier);
+    }
+}
