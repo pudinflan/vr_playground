bb1b49f23ce68adea21f74c78e60c2c9a1b1d463
Fernando Soares <fernando.soares@virtuleap.com>
2022-04-06 19:02:16 +0100

    Implement game scene changing button

---
+++ Assets/_Root/Scripts/SceneManagement/SceneSwitch.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Carnival.SceneManagement
+{
+	public class SceneSwitch : MonoBehaviour
+	{
+		[SerializeField] private string _sceneToLoad;
+
+		public void LoadScene() => SceneManager.LoadScene(_sceneToLoad);
+	}
+}
