bd0d2ba3c500a263bbc6c0a4d84687b0eaad8def
Fernando Soares <fernando.soares@virtuleap.com>
2022-04-07 14:09:32 +0100

    Create Slinger Prefab

---
+++ Assets/_Root/Scripts/Sling/SlingGrabbable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Carnival.Sling
+{
+	public class SlingGrabbable : OVRGrabbable
+	{
+		[Header("Sling Fields")]
+		[SerializeField] private Transform _slingRailTransform;
+		
+		private void Update()
+		{
+			if (!isGrabbed)
+			{
+				return;
+			}
+
+			transform.LookAt(_slingRailTransform);
+		}
+	}
+}
---
+++ Assets/_Root/Scripts/Sling/SlingRail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingRail : MonoBehaviour
+{
+	[SerializeField] private Transform _slingGrabbableTransform;
+
+	private void Update()
+	{
+		transform.rotation = Quaternion.LookRotation (_slingGrabbableTransform.transform.position - transform.position);
+		transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y - 90, 0);
+	}
+}
